bc84db1a44d61a76bc265b7ecb2edd5db751af2f
Johannes von Bargen <jo@Johanness-MacBook-Air.local>
2015-01-21 16:19:20 +0100

    fixes syntax error

--- lib/ducksboard_reporter/reporters/mysql_queries_per_second.rb
+++ lib/ducksboard_reporter/reporters/mysql_queries_per_second.rb
@@ -2,7 +2,7 @@ module DucksboardReporter
   module Reporters
     class MySqlQueriesPerSecond < MySqlBase
       def refresh_current_stats
-        out = `mysqladmin status`.match(/(Queries per second avg: )([-+]?[0-9]*\.?[0-9]+)/)
+        out = `mysqladmin status`.match(/(Queries per second avg: )([-+]?[0-9]*\.?[0-9]+)/)[2]
         out.nil? ? 0 : out
       end
     end
--- lib/ducksboard_reporter/reporters/mysql_slow_queries.rb
+++ lib/ducksboard_reporter/reporters/mysql_slow_queries.rb
@@ -2,7 +2,7 @@ module DucksboardReporter
   module Reporters
     class MySqlSlowQueries < MySqlBase
       def refresh_current_stats
-        out = `mysqladmin status`.match(/(Slow queries: )([-+]?[0-9]*\.?[0-9]+)/[2]
+        out = `mysqladmin status`.match(/(Slow queries: )([-+]?[0-9]*\.?[0-9]+)/)[2]
         out.nil? ? 0 : out
       end
     end
