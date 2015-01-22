89acb21fe590057eeb647d9dbce776123aefbda1
Johannes von Bargen <jo@Johanness-MacBook-Air.local>
2015-01-22 14:05:41 +0100

    fixes row reads data extraction

--- lib/ducksboard_reporter/reporters/mysql_innodb_rows_reads_per_second.rb
+++ lib/ducksboard_reporter/reporters/mysql_innodb_rows_reads_per_second.rb
@@ -2,7 +2,7 @@ module DucksboardReporter
   module Reporters
     class MySqlInnodbRowsReadsPerSecond < MySqlBase
       def refresh_current_stats
-        out = `mysql -e 'SHOW ENGINE INNODB STATUS\\G' |grep 'reads/s'`.match(/(reads\/s)/)[2]
+        out = `mysql -e 'SHOW ENGINE INNODB STATUS\\G'`.scan(/([-+]?[0-9]*\.?[0-9]+) (reads\/s)/)
         out.nil? ? 0 : out
       end
     end
