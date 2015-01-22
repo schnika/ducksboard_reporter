b2a5891903ab3adc64229d3ffb6c2ac4a3cfeecf
Johannes von Bargen <jo@Johanness-MacBook-Air.local>
2015-01-22 12:12:21 +0100

    a backslashhhhh

--- lib/ducksboard_reporter/reporters/mysql_innodb_rows_reads_per_second.rb
+++ lib/ducksboard_reporter/reporters/mysql_innodb_rows_reads_per_second.rb
@@ -2,7 +2,7 @@ module DucksboardReporter
   module Reporters
     class MySqlInnodbRowsReadsPerSecond < MySqlBase
       def refresh_current_stats
-        out = `mysql -e 'SHOW ENGINE INNODB STATUS\G' |grep 'reads/s'`.match(/(reads\/s)/)[2]
+        out = `mysql -e 'SHOW ENGINE INNODB STATUS\\G' |grep 'reads/s'`.match(/(reads\/s)/)[2]
         out.nil? ? 0 : out
       end
     end
